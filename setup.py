12cc6359ff59278f0c98f8a6ec8da3cc863533aa
Marcelo Cure <marcelocure@gmail.com>
2015-05-21 21:08:56 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pml',
+      version='0.0.1',
+      description='Python Messaging Library',
+      author='Marcelo Cure',
+      author_email='marcelocure@gmail.com',
+      url='https://wgithub.com/marcelocure/python-pml/',
+      packages=['pml'],
+      package_dir={'pml': 'src/pml'},
+     )
